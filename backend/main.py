e16304170c7471b0a1d4a586738d0ee846cae502
Sandeep Padmanabhan <sanbustry@gmail.com>
2026-03-24 06:53:54 +0000

    Updated cors

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ logging.basicConfig(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
